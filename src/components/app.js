311a02208643f0dd7c3b70955dd53c23c9dbfd43
Christian Dräger <christian.draeger1@gmail.com>
2018-03-09 18:38:10 +0100

    can handle input events

---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import SearchBar from './search_bar';
+
+const API_KEY = 'AIzaSyDi8aoUWswNLmcJADpoT-l4VUZPvjCKgFs';
+
+export default class App extends Component {
+  render() {
+    return (
+      <SearchBar/>
+    );
+  }
+}
